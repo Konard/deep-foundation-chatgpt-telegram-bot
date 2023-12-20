9b6105d73f3a0ecee748a6df6747e1e607a36a15
Arsentii Bieliaiev <SenchaPencha1@gmail.com>
2023-12-20 05:15:16 +0200

    better readability, split large messages

--- python/main.py
+++ python/main.py
@@ -2,16 +2,17 @@ import asyncio
 import logging
 import os
 import sys
-import traceback
+from typing import Any
 
 import aiofiles
-import httpx
 import openai
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, Router
 from aiogram.enums import ParseMode
-from aiogram.types import BufferedInputFile
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, BufferedInputFile
 from dotenv import load_dotenv
 import tempfile
+from python import Filters as ContentTypesFilter
 
 load_dotenv()
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -21,6 +22,17 @@ openai.api_type = "azure"
 openai.api_key = os.getenv('OPENAI_API_KEY')
 openai.api_base = "https://deep-azure.openai.azure.com/"
 openai.api_version = "2023-06-01-preview"
+
+
+async def send_or_split_message(message, text):
+    text_file = BufferedInputFile(bytes(text, 'utf-8'), filename="file.txt")
+    if len(text) > 4096:
+        for i in range(0, len(text), 4096):
+            text_chunk = text[i:i + 4096]
+            await message.answer(text_chunk, reply_to_message_id=message.message_id)
+    else:
+        await message.answer(text, reply_to_message_id=message.message_id)
+    await message.answer_document(text_file, reply_to_message_id=message.message_id)
 
 
 async def get_openai_completion(prompt):
@@ -37,46 +49,28 @@ async def get_openai_completion(prompt):
         raise
 
 
-dp = Dispatcher()
-
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-# Initialize Bot instance with a default parse mode which will be passed to all API calls
-bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+router = Router(name=__name__)
 
 
-@dp.message()
-async def echo_handler(message: types.Message) -> None:
+@router.message(ContentTypesFilter.Text())
+async def handle_text(message: Message, state: FSMContext) -> Any:
     """
     Handler will forward receive a message back to the sender
 
     By default, message handler will handle all message types (like a text, photo, sticker etc.)
     """
     logger.info(f"---------\nReceived message: {message}")
-    if message.text is not None:
-        if message.reply_to_message is not None:
-            if message.reply_to_message.document is not None:
-                with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-                    await bot.download(message.reply_to_message.document, temp_file.name)
-                    async with aiofiles.open(temp_file.name, 'r', encoding='utf-8') as file:
-                        # Read the content of the file
-                        text = await file.read()
-                promt = "Context: \n" + text + "Promt:\n" + message.text
-                answer = await get_openai_completion(promt)
-                await message.answer(answer, reply_to_message_id=message.message_id)
-                text_file = BufferedInputFile(bytes(answer, 'utf-8'), filename="file.txt")
-                await message.answer_document(text_file, reply_to_message_id=message.message_id)
-            else:
-                promt = "Context\n" + message.reply_to_message.text + "Promt:\n" + message.text
-                answer = await get_openai_completion(promt)
-                await message.answer(answer, reply_to_message_id=message.message_id)
-                text_file = BufferedInputFile(bytes(answer, 'utf-8'), filename="file.txt")
-                await message.answer_document(text_file, reply_to_message_id=message.message_id)
-        else:
-            promt = "Promt:\n" + message.text
-            answer = await get_openai_completion(promt)
-            await message.answer(answer, reply_to_message_id=message.message_id)
-            text_file = BufferedInputFile(bytes(answer, 'utf-8'), filename="file.txt")
-            await message.answer_document(text_file, reply_to_message_id=message.message_id)
+    context_text = message.reply_to_message.text if message.reply_to_message else ""
+    document_file = message.reply_to_message.document if message.reply_to_message and message.reply_to_message.document else None
+
+    if document_file:
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            await bot.download(document_file, temp_file.name)
+
+        async with aiofiles.open(temp_file.name, 'r', encoding='utf-8') as file:
+            context_text += await file.read()
+    answer = await get_openai_completion(f"Context:\n{context_text}Prompt:\n{message.text}")
+    await send_or_split_message(message, answer)
     try:
         # Send a copy of the received message
         logger.info(f"---------\nReceived message: {message}")
@@ -85,8 +79,15 @@ async def echo_handler(message: types.Message) -> None:
         logger.info(f"---------\nReceived message: {message.to_python()}")
 
 
+dp = Dispatcher()
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+# Initialize Bot instance with a default parse mode which will be passed to all API calls
+bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+
+
 async def main() -> None:
-    # And the run events dispatching
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 
